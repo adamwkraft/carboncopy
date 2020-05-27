65a69a5ae2b02136f3f2e77df81af9d01060b215
Robert Reed <robert.mc.reed@gmail.com>
2020-05-26 21:45:40 -0700

    adding bodypix

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,10 +2,20 @@ import React from 'react'
 import { useWebcam } from '../context/webcam';
 import Webcam from './Webcam';
 import WebcamSelect from './WebcamSelect';
+import * as bodyPix from '@tensorflow-models/body-pix';
 
 const Main = (props) => {
-  window.webcam = useWebcam();
+  const webcam = useWebcam();
+  window.webcam = webcam; // TODO: remove
 
+  const makePrediction = async () => {
+    const net = await bodyPix.load();
+    
+    const segmentation = await net.segmentPerson(webcam.canvasRef.current);
+    console.log(segmentation);
+  };
+
+  window.p = makePrediction
 
   return (
     <div>
