7fce40ffa526915a79efed1450c928556574be98
Adam Kraft <adamwkraft@gmail.com>
2020-06-02 21:57:13 -0700

    color stuff

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,23 @@ import Webcam from './Webcam';
 import WebcamSelect from './WebcamSelect';
 import * as bodyPix from '@tensorflow-models/body-pix';
 import { Button } from '@material-ui/core';
+import inside from 'point-in-polygon';
+import { polygonToArray, drawResult, drawResultFlipped} from '../lib/util';
+
+const drawPolygon = (ctx, polygon) => {
+  ctx.fillStyle = 'rgba(255, 255, 255, 0.5)'
+  ctx.beginPath();
+  ctx.moveTo(polygon[0][0], polygon[0][1]);
+  const vertices = polygon.slice(1);
+  vertices.forEach(([x, y]) => {
+    ctx.lineTo(x, y);
+  });
+  ctx.closePath();
+  ctx.fill();
+}
+
+const flipPolygon = (polygon, width) => polygon.map(([x, y]) => [width - x, y])
+
 
 const Main = (props) => {
   const webcam = useWebcam();
@@ -12,13 +29,37 @@ const Main = (props) => {
 
   const makePrediction = async () => {
     const segmentation = await net.current.segmentPerson(webcam.videoRef.current);
+    console.log("Segmentation!", segmentation);
     const coloredPartImage = bodyPix.toMask(segmentation);
     const opacity = 0.7;
     const maskBlurAmount = 0;
 
-    bodyPix.drawMask(
-     webcam.canvasRef.current, webcam.videoRef.current, coloredPartImage, opacity, maskBlurAmount,
-      webcam.flipX);
+    const template = [
+      [0, 0],
+      [webcam?.canvasRef?.current?.width/2, 0],
+      [webcam?.canvasRef?.current?.width/2, webcam?.canvasRef?.current?.height],
+      [0, webcam?.canvasRef?.current?.height]];
+
+    const flippedTemplate = flipPolygon(template, webcam?.canvasRef?.current?.width);
+    
+    const ctx = webcam.canvasRef.current.getContext('2d');
+    // Create an array from the template.
+    // const image = polygonToArray(template,
+    //   webcam?.canvasRef?.current?.width,
+    //   webcam?.canvasRef?.current?.height);
+    const image = drawResultFlipped(
+      flippedTemplate,
+      segmentation)
+
+    ctx.putImageData(image, 0, 0);
+
+    // bodyPix.drawMask(
+    //  webcam.canvasRef.current, webcam.videoRef.current, coloredPartImage, opacity, maskBlurAmount,
+    //   webcam.flipX);
+
+
+    // drawPolygon(ctx, template);
+    // ctx.fillRect(0, 0, webcam.canvasRef.current.width/2, webcam.canvasRef.current.height);
 
     if (running.current) {
       requestAnimationFrame(makePrediction);
@@ -27,8 +68,6 @@ const Main = (props) => {
       webcam.clearCanvas();
     }
   };
-
-  window.p = makePrediction
 
   const handleClickStartAButtonPressFun = async () => {
     if (!net.current) {
@@ -50,10 +89,6 @@ const Main = (props) => {
         <Button variant='contained' onClick={handleClickStopAButtonPressFun}>Stop</Button>
       </div>
       <Webcam width={1280} withCanvas={true} />
-      <canvas
-          width={500}
-          height={500}
-        />
     </div>
   );
 };
---
+++ src/lib/util.js
@@ -0,0 +1,55 @@
+import inside from 'point-in-polygon';
+
+export const polygonToArray = (polygon, width, height) => {
+    const bytes = new Uint8ClampedArray(width * height * 4);
+    for (let i = 0; i < height * width; ++i) {
+      const x = i % width;
+      const y = parseInt(i / width);
+      const isIn = Number(inside([x, y], polygon)) * 255;
+      bytes[i*4] = isIn;
+      bytes[i*4+1] = isIn;
+      bytes[i*4+2] = isIn;
+      bytes[i*4+3] = isIn ? 128 : 0;
+    }
+    return new ImageData(bytes, width, height);
+}
+
+
+export const drawResult = (polygon, segmentation) => {
+  const bytes = new Uint8ClampedArray(segmentation.data.length * 4);
+  const {data, width, height} = segmentation; 
+  for (let i = 0; i < height * width; ++i) {
+    const x = i % width;
+    const y = parseInt(i / width);
+    const isIn = inside([x, y], polygon)
+    const isPerson = data[i];
+    const isIntersection = isIn && isPerson;
+    bytes[i*4] = isIntersection * 255;
+    bytes[i*4+1] = isIntersection * 255;
+    bytes[i*4+2] = isIntersection * 255;
+    bytes[i*4+3] = isIntersection ? 128 : 0;
+  }
+  return new ImageData(bytes, width, height);
+
+}
+
+export const drawResultFlipped = (polygon, segmentation) => {
+  const bytes = new Uint8ClampedArray(segmentation.data.length * 4);
+  const {data, width, height} = segmentation; 
+  for (let i = 0; i < height * width; ++i) {
+    const x = i % width;
+    const y = parseInt(i / width);
+    const isIn = inside([x, y], polygon)
+    const isPerson = data[i];
+    const isIntersection = isIn && isPerson;
+    const isMissedPolygon = isIn && !isPerson;
+    const isFalseDetection = !isIn && isPerson;
+    const bytes_index  = (width - x) + (width * y);
+    bytes[bytes_index*4] = isFalseDetection ? 255 : 0;
+    bytes[bytes_index*4+1] = isIntersection? 255 : 0;
+    bytes[bytes_index*4+2] = isMissedPolygon ? 255 : 0;
+    bytes[bytes_index*4+3] = 128;
+  }
+  return new ImageData(bytes, width, height);
+
+}
