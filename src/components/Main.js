2334b49ec54213cc5fe9800025cf7b4b5d5e9a54
Adam Kraft <adamwkraft@gmail.com>
2020-06-02 19:53:36 -0700

    add fun buttons

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,34 +1,59 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { useWebcam } from '../context/webcam';
 import Webcam from './Webcam';
 import WebcamSelect from './WebcamSelect';
 import * as bodyPix from '@tensorflow-models/body-pix';
+import { Button } from '@material-ui/core';
 
 const Main = (props) => {
   const webcam = useWebcam();
-  window.webcam = webcam; // TODO: remove
+  const net = useRef();
+  const running = useRef();
 
   const makePrediction = async () => {
-    const net = await bodyPix.load();
-    
-    const segmentation = await net.segmentPerson(webcam.videoRef.current);
+    const segmentation = await net.current.segmentPerson(webcam.videoRef.current);
     const coloredPartImage = bodyPix.toMask(segmentation);
     const opacity = 0.7;
-    const flipHorizontal = false;
     const maskBlurAmount = 0;
 
     bodyPix.drawMask(
      webcam.canvasRef.current, webcam.videoRef.current, coloredPartImage, opacity, maskBlurAmount,
-      flipHorizontal);
-    console.log(segmentation);
+      webcam.flipX);
+
+    if (running.current) {
+      requestAnimationFrame(makePrediction);
+    } else{
+      // Clear canvas.
+      webcam.clearCanvas();
+    }
   };
 
   window.p = makePrediction
 
+  const handleClickStartAButtonPressFun = async () => {
+    if (!net.current) {
+      net.current = await bodyPix.load();
+    }
+    running.current = true;
+    makePrediction();
+  }
+
+  const handleClickStopAButtonPressFun = async () => {
+    running.current = false;
+  }
+
   return (
     <div>
-      <WebcamSelect />
+      <div>
+        <WebcamSelect />
+        <Button variant='contained' onClick={handleClickStartAButtonPressFun}>Start</Button>
+        <Button variant='contained' onClick={handleClickStopAButtonPressFun}>Stop</Button>
+      </div>
       <Webcam width={1280} withCanvas={true} />
+      <canvas
+          width={500}
+          height={500}
+        />
     </div>
   );
 };
