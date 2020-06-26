888132533489d63e5c7df9ef64e906e217fd3b3d
Robert Reed <robert.mc.reed@gmail.com>
2020-06-25 23:11:34 -0700

    loop time manager

---
+++ src/hooks/useLoopTimer.js
@@ -0,0 +1,58 @@
+import { useCallback, useRef, } from "react";
+import { useMemo } from "react";
+
+export const useLoopTimer = () => {
+  const lastTimeRef = useRef();
+  const startTimeRef = useRef();
+  const totalFramesRef = useRef();
+  const fpsSumRef = useRef();
+  const timerRef = useRef();
+
+  const reset = useCallback((timestamp=0) => {
+    startTimeRef.current = timestamp;
+      lastTimeRef.current = timestamp;
+      fpsSumRef.current = 0;
+      totalFramesRef.current = 0;
+      timerRef.current = 0;
+  }, []);
+
+  const resetTimer = useCallback(() => {
+    timerRef.current = 0;
+  }, []);
+  
+  const update = useCallback((timestamp) => {
+    if (!startTimeRef.current) {
+      reset(timestamp);
+    }
+
+    const deltaTime = timestamp - lastTimeRef.current;
+    timerRef.current += deltaTime;
+
+    let fps = 0;
+    let avgFps = 0;
+
+    // start calculating after the first loop to avoid dividing by 0
+    if (totalFramesRef.current++) {
+      fps = 1000 / deltaTime;
+      avgFps = fpsSumRef.current / totalFramesRef.current;
+      fpsSumRef.current += fps;
+    }
+    
+    const elapsed = timestamp - startTimeRef.current;
+    lastTimeRef.current = timestamp;
+
+    return {
+      timestamp,
+      fps,
+      avgFps,
+      elapsed,
+      frames: totalFramesRef.current,
+      timer: timerRef.current,
+      resetTimer,
+    };
+  }, [reset, resetTimer]);
+
+  const controller = useMemo(() => ({ update, reset }), [update, reset]);
+
+  return controller;
+};
