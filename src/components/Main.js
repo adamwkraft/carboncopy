e0cf7c06986b0066aec23a24a00662044640cbf5
Robert Reed <robert.mc.reed@gmail.com>
2020-06-25 23:12:14 -0700

    use loop and test handler in main

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,12 +2,12 @@ import React, { useCallback } from 'react'
 import { makeStyles } from '@material-ui/core';
 
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 
 import Webcam from './Webcam';
 import WebcamSelect from './WebcamSelect';
 
-import { useMainLoop } from '../hooks/mainLoop';
+import { useLoop } from '../hooks/loop';
+import { useTestLoopHandler } from '../hooks/testLoopHandler';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -16,19 +16,19 @@ const useStyles = makeStyles(theme => ({
   },
   options: {
     display: 'flex',
+    alignItems: 'center',
     flexDirection: 'row-reverse',
     justifyContent: 'space-between',
-    alignItems: 'center',
     marginBottom: theme.spacing(1),
   },
-  score: {
-
-  }
 }));
 
 const Main = (props) => {
-  const loop = useMainLoop();
   const classes = useStyles();
+  
+  const handleLoop = useTestLoopHandler();
+
+  const loop = useLoop(handleLoop);
 
   const handleClickStartStop = useCallback(async () => (loop.looping
     ? loop.stop()
@@ -47,32 +47,8 @@ const Main = (props) => {
         >
           { loop.looping ? 'Stop' : 'Start' }
         </Button>
-        <Button
-          color='primary'
-          variant='contained'
-          disabled={!loop.ready}
-          onClick={loop.startCaptureSegmentations}
-        >
-          Capture Segmentations
-        </Button>
-        <Button
-          color='primary'
-          variant='contained'
-          disabled={!loop.ready}
-          onClick={loop.captureSegmentationAsDataURI}
-        >
-          Capture and Save Segmentation
-        </Button>
       </div>
       <Webcam />
-      { loop.looping && (
-        <Typography variant="h4">
-          Polygon Score: {loop.score}
-        </Typography>
-      )}
-      <Typography variant="h4">
-        Scores: {loop.scores.join(', ')} = {loop.scores.reduce((a,b) => a+b,0)}
-      </Typography>
     </div>
   );
 };
