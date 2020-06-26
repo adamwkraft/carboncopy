2a5e783ece27e445188c1ec12b46cf239198e542
Robert Reed <robert.mc.reed@gmail.com>
2020-06-25 23:11:47 -0700

    use timer in loop

--- src/hooks/loop.js
+++ src/hooks/loop.js
@@ -2,34 +2,40 @@ import { useRef, useState, useCallback, useMemo } from "react";
 
 import { useBodyPix } from "./bodyPix";
 import { useWebcam } from "../context/webcam";
+import { useLoopTimer } from "./useLoopTimer";
 
-export const useMainLoop = (handleLoop) => {
+export const useLoop = (handleLoop) => {
   const webcam = useWebcam();
   const predict = useBodyPix();
 
+  const timer = useLoopTimer();
   const loopingRef = useRef();
   const [looping, setLooping] = useState(false);
 
   const setStartLoop = useCallback(() => {
+    timer.reset();
     loopingRef.current = true;
     setLooping(true);
-  }, []);
+  }, [timer]);
   
   const setStopLoop = useCallback(() => {
     loopingRef.current = false;
     setLooping(false);
   }, []);
 
-  const loop = useCallback(async () => {
-    const finished = await handleLoop({ webcam, predict });
+  const loop = useCallback(async (timestamp) => {
+    const time = timer.update(timestamp);
+    const cleanup = await handleLoop({ webcam, predict, time, stop: setStopLoop });
 
-    if (loopingRef.current && !finished) {
+    if (loopingRef.current) {
       requestAnimationFrame(loop);
     } else{
       setStopLoop();
       webcam.clearCanvas();
+      if (cleanup) cleanup();
     }
   }, [
+    timer,
     webcam,
     predict,
     handleLoop,
@@ -52,7 +58,7 @@ export const useMainLoop = (handleLoop) => {
 
     setStartLoop();
 
-    return loop();
+    return requestAnimationFrame(loop);
   }, [loop, handleLoop, predict, setStartLoop]);
 
   const controller = useMemo(() => ({
