c675dcab601827b2236d28f04d48fb62ed96e855
Robert Reed <robert.mc.reed@gmail.com>
2020-06-26 23:21:40 -0700

    add Css baseline

--- src/App/App.js
+++ src/App/App.js
@@ -4,6 +4,7 @@ import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
 
 import Main from '../components/Main';
 import WebcamProvider from '../context/webcam';
+import { CssBaseline } from '@material-ui/core';
 
 const theme = createMuiTheme({
   background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
@@ -13,7 +14,10 @@ function App() {
   return (
     <MuiThemeProvider theme={theme}>
       <WebcamProvider>
-        <Main />
+        <>
+          <CssBaseline />
+          <Main />
+        </>
       </WebcamProvider>
     </MuiThemeProvider>
   );
