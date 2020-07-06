00f943ffcaab9852d3d6a5d3180844214e438274
Robert Reed <robert.mc.reed@gmail.com>
2020-07-05 21:52:10 -0700

    move bodypix to worker thread

--- src/hooks/bodyPixController.js
+++ src/hooks/bodyPixController.js
@@ -1,27 +1,28 @@
-import * as bodyPix from '@tensorflow-models/body-pix';
 import { useRef, useState, useEffect, useCallback } from "react";
 
 import { useWebcam } from "../context/webcam";
+
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import BodyPixWorker from 'workerize-loader!../worker/bodyPix';
+
+const bodyPixWorker = BodyPixWorker();
 
 export const useBodyPixController = () => {
   const webcam = useWebcam();
 
   const loadRef = useRef();
-  const [net, setNet] = useState(null);
+  const preloadRef = useRef();
+  const [netReady, setNetReady] = useState(false);
+  const [netPreloaded, setNetPreloaded] = useState(false);
 
   // load the model on the first mount
   useEffect(() => {
     if (!loadRef.current && webcam.ready) {
       loadRef.current = true;
-      bodyPix.load({
-        architecture: 'ResNet50', //'MobileNetV1',
-        outputStride: 16,
-        multiplier: 1,
-        quantBytes: 4,
-      })
-        .then((loadedNet) => {
+      bodyPixWorker.load()
+        .then(() => {
           console.log('BodyPix loaded');
-          setNet(loadedNet);
+          setNetReady(true);
         })
         .catch(console.error);
     }
@@ -29,16 +30,28 @@ export const useBodyPixController = () => {
 
   // TODO: Make it possible to tweak bodyPix settings
   const predict = useCallback(async () => {
-    if (!net) {
+    if (!netReady) {
       throw new Error('Please wait for the model to load before calling "predict"');
     }
 
-    const segmentation = await net.segmentPerson(webcam.videoRef.current, {internalResolution: 'medium'});
+    const segmentation = await bodyPixWorker.predict(webcam.getVideoAsImageData());
 
     // console.log({segmentation});
     
     return segmentation;
-  }, [net, webcam]);
+  }, [netReady, webcam]);
 
-  return net ? predict : null;
+  // pre load the model by running a first prediction
+  useEffect(() => {
+    if (netReady && !preloadRef.current && !netPreloaded) {
+      preloadRef.current = true;
+      predict()
+        .then(() => {
+          setNetPreloaded(true);
+        });
+    }
+  }, [predict, netReady, netPreloaded]);
+
+  // don't return the prediction fn until the model has processed the first frame
+  return netPreloaded ? predict : null;
 };
