55e3729772d11dc25f60689b3b9f698b0ab86a96
Robert Reed <robert.mc.reed@gmail.com>
2020-07-05 18:51:34 -0700

    move bodypix to context

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
+import { CssBaseline } from '@material-ui/core';
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
 
 import Main from '../components/Main';
 import WebcamProvider from '../context/webcam';
-import { CssBaseline } from '@material-ui/core';
+import BodyPixProvider from '../context/bodyPix';
 
 const theme = createMuiTheme({
   background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
@@ -14,10 +15,12 @@ function App() {
   return (
     <MuiThemeProvider theme={theme}>
       <WebcamProvider>
-        <>
-          <CssBaseline />
-          <Main />
-        </>
+        <BodyPixProvider>
+          <>
+            <CssBaseline />
+            <Main />
+          </>
+        </BodyPixProvider>
       </WebcamProvider>
     </MuiThemeProvider>
   );
