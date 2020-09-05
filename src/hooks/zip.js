e96c7ffaac2e3260e0cf29a1e651728b23b3d9e4
Robert Reed <robert.mc.reed@gmail.com>
2020-09-05 16:50:07 -0700

    zip loading hooks

---
+++ src/hooks/zip.js
@@ -0,0 +1,71 @@
+import JSZip from 'jszip';
+import JSZipUtils from 'jszip-utils';
+import { useCallback, useState, useMemo } from 'react';
+import { useWebcam } from '../context/webcam';
+
+export const useZip = (setMasks) => {
+  const webcam = useWebcam();
+  const [zipLoading, setZipLoading] = useState(false);
+
+  const loadZippedMasks = useCallback(
+    async (file) => {
+      const data = await JSZip.loadAsync(file);
+      const binaryMasks = await Promise.all(
+        data
+          .filter((name) => name.endsWith('.png'))
+          .map(({ name }) => data.file(name).async('base64')),
+      );
+
+      try {
+        const masksAsImageData = await Promise.all(
+          binaryMasks.map((b64) => webcam.dataUriToImageData(`data:image/png;base64,${b64}`)),
+        );
+        setMasks(masksAsImageData);
+      } catch {}
+
+      setZipLoading(false);
+    },
+    [setMasks, webcam],
+  );
+
+  const handleZipInputChange = useCallback(
+    async ([file]) => {
+      if (file.type !== 'application/zip') {
+        console.error('Expected a zip file but got', file.type);
+        return;
+      }
+
+      setZipLoading(true);
+      await loadZippedMasks(file);
+    },
+    [loadZippedMasks],
+  );
+
+  const handleLoadPreparedMasks = useCallback(
+    async (filename) => {
+      if (!filename) return;
+
+      setZipLoading(true);
+      const file = await new JSZip.external.Promise((resolve, reject) => {
+        JSZipUtils.getBinaryContent(process.env.PUBLIC_URL + `/masks/${filename}`, (err, data) => {
+          if (err) reject(err);
+          else resolve(data);
+        });
+      });
+
+      await loadZippedMasks(file);
+    },
+    [loadZippedMasks],
+  );
+
+  const data = useMemo(
+    () => ({
+      loading: zipLoading,
+      handleZipInputChange,
+      handleLoadPreparedMasks,
+    }),
+    [zipLoading, handleZipInputChange, handleLoadPreparedMasks],
+  );
+
+  return data;
+};
