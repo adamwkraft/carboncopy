3c568c9853c3fff2ac680ca07d15b25e993d33ab
Robert Reed <robert.mc.reed@gmail.com>
2020-09-07 14:33:55 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rob_and_adam_code_a_thing/precache-manifest.b15ddbb9fbe10c8e8670a846788f9663.js"
+  "/rob_and_adam_code_a_thing/precache-manifest.383eade75cb13c9d604cae886b72c534.js"
 );
 
 self.addEventListener('message', (event) => {
