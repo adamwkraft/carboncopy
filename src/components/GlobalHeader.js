c01acaec635da49437e62f5fc9b68f98f36e4fe4
Robert Reed <robert.mc.reed@gmail.com>
2020-12-10 00:24:27 -0800

    header work

--- src/components/GlobalHeader.js
+++ src/components/GlobalHeader.js
@@ -3,8 +3,6 @@ import classnames from 'classnames';
 import React, { memo, useState, useCallback } from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import { makeStyles } from '@material-ui/styles';
-import { withStyles } from '@material-ui/core/styles';
-import HomeIcon from '@material-ui/icons/Home';
 import BackIcon from '@material-ui/icons/ArrowLeft';
 import IconButton from '@material-ui/core/IconButton';
 import VolumeUpIcon from '@material-ui/icons/VolumeUp';
@@ -20,14 +18,15 @@ import MuiDialogTitle from '@material-ui/core/DialogTitle';
 import MuiDialogContent from '@material-ui/core/DialogContent';
 import CloseIcon from '@material-ui/icons/Close';
 import Typography from '@material-ui/core/Typography';
-import stickImage from '../images/stick_figure_in_box.png';
+import ContactSupportIcon from '@material-ui/icons/ContactSupport';
 
 const useStyles = makeStyles((theme) => ({
   btn: {
     border: '2px solid grey',
+    marginLeft: theme.spacing(1),
   },
   back: {
-    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(1),
   },
   options: {
     marginTop: theme.spacing(2),
@@ -45,58 +44,16 @@ const useStyles = makeStyles((theme) => ({
     alignItems: 'center',
     justifyContent: 'space-between',
   },
-  volume: {
-    marginLeft: theme.spacing(1),
-  },
   closeButton: {
     position: 'absolute',
     right: theme.spacing(1),
     top: theme.spacing(1),
-    color: theme.palette.grey[500],
   },
-  info: {
-    marginLeft: theme.spacing(2),
-    width: '34px',
-    height: '34px',
-  },
-  stickImage: {
-    width: '100%',
+  ever: {
+    fontWeight: 'bold',
+    fontStyle: 'italic',
   },
 }));
-
-const styles = (theme) => ({
-  root: {
-    margin: 0,
-    padding: theme.spacing(2),
-  },
-  closeButton: {
-    position: 'absolute',
-    right: theme.spacing(1),
-    top: theme.spacing(1),
-    color: theme.palette.grey[500],
-  },
-});
-
-const DialogTitle = withStyles(styles)((props) => {
-  const { children, classes, onClose, ...other } = props;
-  return (
-    <MuiDialogTitle disableTypography className={classes.root} {...other}>
-      <Typography variant="h6">{children}</Typography>
-      {onClose ? (
-        <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
-          <CloseIcon />
-        </IconButton>
-      ) : null}
-    </MuiDialogTitle>
-  );
-});
-
-const DialogContent = withStyles((theme) => ({
-  root: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-  },
-}))(MuiDialogContent);
 
 const GlobalHeader = (props) => {
   const classes = useStyles();
@@ -112,60 +69,84 @@ const GlobalHeader = (props) => {
   const styleProps = useSpring({ to: { opacity: !onHomeScreen ? 1 : 0 }, config: config.stiff });
 
   return (
-    <div className={classes.options}>
-      <div>
-        {!onHomeScreen && (
-          <animated.div style={styleProps}>
-            <IconButton className={classes.btn} size="small" onClick={props.goHome}>
-              <HomeIcon />
-            </IconButton>
-            <IconButton
-              size="small"
-              className={classnames(classes.btn, classes.back)}
-              onClick={
-                props.players === screenStates.players.MULTIPLAYER ? props.goHome : props.goBack
-              }
-            >
-              <BackIcon />
-            </IconButton>
-            <IconButton
-              size="small"
-              className={classnames(classes.btn, classes.info)}
-              onClick={() => {
-                setOpen(true);
-              }}
-            >
-              <b>i</b>
-            </IconButton>
-            <Dialog
-              onClose={handleClose}
-              aria-labelledby="customized-dialog-title"
-              open={open || !props.tutorial.completed}
-            >
-              <DialogTitle id="customized-dialog-title" onClose={handleClose}>
-                Set Up
-              </DialogTitle>
-              <DialogContent dividers>
-                <Typography gutterBottom>
-                  Place your webcam at a distance and ensure there is enough space around you to
-                  move safely. Your whole body should be in view of your webcam.
-                </Typography>
-                <Typography gutterBottom>
-                  No personal images or data are collected by this application.
-                </Typography>
-                {/* <img src={stickImage} className={classes.stickImage} alt="Stick Figure" /> */}
-              </DialogContent>
-            </Dialog>
-          </animated.div>
-        )}
+    <>
+      <div className={classes.options}>
+        <div className={classes.left}></div>
+        <div className={classes.right}>
+          {!onHomeScreen && (
+            <animated.div style={styleProps}>
+              <IconButton
+                size="small"
+                className={classnames(classes.btn, classes.back)}
+                onClick={
+                  props.players === screenStates.players.MULTIPLAYER ? props.goHome : props.goBack
+                }
+              >
+                <BackIcon />
+              </IconButton>
+            </animated.div>
+          )}
+          <WebcamSelect />
+          <IconButton
+            className={classnames(classes.btn, classes.volume)}
+            size="small"
+            onClick={audio.handlers.toggle}
+          >
+            {audio.state ? <VolumeUpIcon /> : <VolumeOffIcon />}
+          </IconButton>
+          <IconButton
+            size="small"
+            className={classnames(classes.btn)}
+            onClick={() => {
+              setOpen(true);
+            }}
+          >
+            <ContactSupportIcon />
+          </IconButton>
+        </div>
       </div>
-      <div className={classes.right}>
-        <WebcamSelect />
-        <IconButton className={classes.volume} size="small" onClick={audio.handlers.toggle}>
-          {audio.state ? <VolumeUpIcon /> : <VolumeOffIcon />}
-        </IconButton>
-      </div>
-    </div>
+      <Dialog
+        onClose={handleClose}
+        aria-labelledby="information"
+        open={open || !props.tutorial.completed}
+        className={classes.dialogRoot}
+      >
+        <MuiDialogTitle disableTypography id="information">
+          <Typography variant="h5">Carbon Copy Fun!</Typography>
+          <IconButton aria-label="close" className={classes.closeButton} onClick={handleClose}>
+            <CloseIcon />
+          </IconButton>
+        </MuiDialogTitle>
+        <MuiDialogContent dividers className={classes.dialogContent}>
+          <Typography component="h2" variant="h6">
+            Setup
+          </Typography>
+          <Typography gutterBottom>
+            Place your webcam so that your entire body fits within the frame, and ensure there is
+            enough space around you to move safely.
+          </Typography>
+          <Typography gutterBottom>
+            Be prepared to run side to side, forward and back, and even to jump and crouch.
+          </Typography>
+          <Typography component="h2" variant="h6">
+            Performance
+          </Typography>
+          <Typography gutterBottom>
+            This game relies on state-of-the-art computer vision algorithms in order to detect the
+            location of your body. This requires significant compute power. Don't be afraid if you
+            hear your computer fan turn on. Trust that it's trying its hardest to deliver you the
+            best game possible.
+          </Typography>
+          <Typography component="h2" variant="h6">
+            Privacy
+          </Typography>
+          <Typography gutterBottom>
+            Although this game requires your webcam to function, no personal images or data are
+            collected by this application. <span className={classes.ever}>Ever!</span>
+          </Typography>
+        </MuiDialogContent>
+      </Dialog>
+    </>
   );
 };
 
