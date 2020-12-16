6882d7985df241c07870fa5c4aed52c2860e8e95
Robert Reed <robert.mc.reed@gmail.com>
2020-12-15 22:38:32 -0800

    css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-import './reset.css';
+import './index.css';
 import App from './App/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
