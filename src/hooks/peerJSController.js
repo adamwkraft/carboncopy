0fb1b8c50eb59d5aba4d4f583127bcc39e42d460
Robert Reed <robert.mc.reed@gmail.com>
2021-05-18 21:41:17 -0700

    fix typos

--- src/hooks/peerJSController.js
+++ src/hooks/peerJSController.js
@@ -2,18 +2,21 @@ import Peer from 'peerjs';
 import { useState } from 'react';
 
 export const usePeerJSController = () => {
-  const [peer, setPeer] = useState();
-  const [connection, setConnection] = useState(false);
+  const [peer, setPeer] = useState(null);
+  const [myId, setId] = useState(null);
+  const [connection, setConnection] = useState(null);
 
   const init = () => {
     const _peer = new Peer();
-    setPeer(peer);
+    setPeer(_peer);
 
     _peer.on('open', (id) => {
       console.log('Peer id is:', id);
+      setId(id);
     })
 
     _peer.on('connection', (conn) => {
+      console.log('Connection request:', conn)
       setConnection(conn);
 
       conn.on('open', () => {
@@ -24,15 +27,20 @@ export const usePeerJSController = () => {
         conn.send('Hey there, bub!');
       });
     });
+
+    _peer.on('error', err => {
+      console.log(err);
+    })
   }
 
   const connect = (id) => {
-    peer.conenct(id);
+    console.log('Connecting to peer:', id);
+    peer.connect(id);
   }
 
   const send = (message) => {
     connection.send(message)
   }
 
-  return { init, connect, send, peer, connection };
+  return { init, connect, send, peer, connection, peerId: myId };
 };
