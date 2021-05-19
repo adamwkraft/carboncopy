74ad1d6ba76b8b6507a0cf14f29b19da7758b823
Robert Reed <robert.mc.reed@gmail.com>
2021-05-18 21:26:58 -0700

    quick peer component to test

---
+++ src/components/PeerTemp.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/styles';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { usePeerJS } from '../context/peer';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
+  text: {
+    flexGrow: 1
+  }
+}));
+
+const PeerTemp = (props) => {
+  const classes = useStyles();
+  const peerJs = usePeerJS();
+  const [peerId, setPeerId] = useState('');
+  const [message, setMessage] = useState('');
+
+  return (
+    <div className={classes.root}>
+      <Button disabled={peerJs.peer} variant="outlined" onClick={peerJs.init}>Initialize</Button>
+      <TextField className={classes.text} disabled={!peerJs.connection} onChange={e => setPeerId(e.target.value)} />
+      <Button disabled={!peerJs.connection || !peerId} variant="outlined" onClick={() => peerJs.connect(peerId)}>Connect</Button>
+      <TextField className={classes.text} disabled={!peerJs.connection} onChange={e => setMessage(e.target.value)} />
+      <Button variant="outlined" disabled={!peerJs.connection} onClick={() => {
+        peerJs.send(message);
+        setMessage('');
+      }}>Send Message</Button>
+    </div>
+  );
+};
+
+PeerTemp.propTypes = {};
+
+export default PeerTemp;
