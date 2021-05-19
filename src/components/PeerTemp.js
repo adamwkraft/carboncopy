fca6a0b6334dfc75cbdd44e988585fa14f32c4e6
Robert Reed <robert.mc.reed@gmail.com>
2021-05-18 21:41:25 -0700

    fix disabled prop

--- src/components/PeerTemp.js
+++ src/components/PeerTemp.js
@@ -19,12 +19,13 @@ const PeerTemp = (props) => {
   const peerJs = usePeerJS();
   const [peerId, setPeerId] = useState('');
   const [message, setMessage] = useState('');
+  console.log(peerJs);
 
   return (
     <div className={classes.root}>
-      <Button disabled={peerJs.peer} variant="outlined" onClick={peerJs.init}>Initialize</Button>
-      <TextField className={classes.text} disabled={!peerJs.connection} onChange={e => setPeerId(e.target.value)} />
-      <Button disabled={!peerJs.connection || !peerId} variant="outlined" onClick={() => peerJs.connect(peerId)}>Connect</Button>
+      <Button disabled={!!peerJs.peer} variant="outlined" onClick={peerJs.init}>Initialize</Button>
+      <TextField className={classes.text} disabled={!peerJs.peer || !!peerJs.connection} onChange={e => setPeerId(e.target.value)} />
+      <Button disabled={!peerJs.peer || !peerId || !!peerJs.connection} variant="outlined" onClick={() => peerJs.connect(peerId)}>Connect</Button>
       <TextField className={classes.text} disabled={!peerJs.connection} onChange={e => setMessage(e.target.value)} />
       <Button variant="outlined" disabled={!peerJs.connection} onClick={() => {
         peerJs.send(message);
