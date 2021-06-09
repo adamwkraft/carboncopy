165027da2c456c27f929d29f2459f897dee524e3
Robert Reed <robert.mc.reed@gmail.com>
2021-06-08 19:52:37 -0700

    feat: peer utils

---
+++ src/lib/peerUtils.js
@@ -0,0 +1,12 @@
+import faker from 'faker';
+
+import {PEER_ID_BASE} from '../lib/constants';
+
+export const makePeerId = id => PEER_ID_BASE + id;
+
+export const cleanPeerId = id => id.replace(PEER_ID_BASE, '');
+
+export const makeNameOk = name => name.replace(/[^a-zA-Z0-9]/g, '');
+
+export const getRandomId = () => faker.internet.userName();
+
