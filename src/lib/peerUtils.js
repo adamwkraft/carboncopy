c566f6a92ebbf70695f884cc2b6d7b4798931fc2
Robert Reed <robert.mc.reed@gmail.com>
2021-06-08 20:32:22 -0700

    fix: allow null peerId in clean fn

--- src/lib/peerUtils.js
+++ src/lib/peerUtils.js
@@ -4,7 +4,7 @@ import {PEER_ID_BASE} from '../lib/constants';
 
 export const makePeerId = id => PEER_ID_BASE + id;
 
-export const cleanPeerId = id => id.replace(PEER_ID_BASE, '');
+export const cleanPeerId = id => id?.replace(PEER_ID_BASE, '');
 
 export const makeNameOk = name => name.replace(/[^a-zA-Z0-9]/g, '');
 
