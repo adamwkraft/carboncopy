a20dc8c9150d667019bbd1fd8319ea15e409676c
Robert Reed <robert.mc.reed@gmail.com>
2021-07-01 22:31:45 -0700

    Updates

---
+++ precache-manifest.dca061b250e384445b838c943ff254f4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "489e995a70601c77a0dca3243345fa9b",
+    "url": "/2a09d82d70ebff7a9f2b.worker.js"
+  },
+  {
+    "revision": "438689257092c5bebb80cfc1346a670c",
+    "url": "/2a09d82d70ebff7a9f2b.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "d51c686fc10ba89020af3ceec1c37327",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd073a91acb7e1d341ee",
+    "url": "/static/css/main.ba07ecb9.chunk.css"
+  },
+  {
+    "revision": "c826b0a7185f4fa88627",
+    "url": "/static/js/2.ff27f12e.chunk.js"
+  },
+  {
+    "revision": "872a8e203df4fdaf86245a8a1c8b580a",
+    "url": "/static/js/2.ff27f12e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd073a91acb7e1d341ee",
+    "url": "/static/js/main.0a0d9e19.chunk.js"
+  },
+  {
+    "revision": "0f09572e608343d2da24",
+    "url": "/static/js/runtime-main.542c5c4d.js"
+  },
+  {
+    "revision": "e400cec2bf181ca4251a8ac7ed388c6e",
+    "url": "/static/media/logo512.e400cec2.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6997496f5c6778da3c19644debe83ac1.js"
+  "/precache-manifest.dca061b250e384445b838c943ff254f4.js"
 );
 
 self.addEventListener('message', (event) => {
